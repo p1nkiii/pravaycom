bfde248f4a211025ac7fa199e1a7f9166403daec
keithincharge <keithakpalo@gmail.com>
2025-10-21 18:20:05 +0200

    ewrr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KVQHE4ZH3Q"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-KVQHE4ZH3Q');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
