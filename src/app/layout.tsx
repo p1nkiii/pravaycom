822ad4a1621c8f272ad8660517947a6d17219584
keithincharge <keithakpalo@gmail.com>
2025-11-01 12:00:14 +0100

    ewfewr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AppHeader from "@/components/AppHeader";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
         
         <AppHeader />
         {children}
+        <Analytics />
       </body>
     </html>
   );
